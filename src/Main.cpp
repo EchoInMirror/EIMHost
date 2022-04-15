60a49f763fe18d39e13957f8c69e59be29f7078c
Shirasawa <764798966@qq.com>
2022-04-15 18:40:39 +0800

    Update

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,16 @@
-#include <juce_core/juce_core.h>
+#include <juce_audio_processors/juce_audio_processors.h>
 
 int main (int argc, char* argv[]) {
-
-    // Your code goes here!
-    juce::ignoreUnused (argc, argv);
-
+    juce::initialiseJuce_GUI();
+    juce::AudioPluginFormatManager manager;
+    manager.addDefaultFormats();
+    juce::OwnedArray<juce::PluginDescription> results;
+    for (auto it : manager.getFormats()) it->findAllTypesForFile(results, juce::StringArray(argv, argc).joinIntoString(" "));
+    if (results.isEmpty()) return 1;
+    juce::XmlElement::TextFormat format;
+    format.newLineChars = nullptr;
+    format.addDefaultHeader = false;
+    for (auto it : results) puts(it->createXml()->toString(format).toRawUTF8());
+    juce::shutdownJuce_GUI();
     return 0;
 }
