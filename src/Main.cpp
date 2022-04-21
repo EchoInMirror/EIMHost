5296f938753002a0f05ff466b2609b0d4500e8a1
Shirasawa <764798966@qq.com>
2022-04-21 12:33:18 +0800

    Update

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,7 +6,8 @@ int main (int argc, char* argv[]) {
     manager.addDefaultFormats();
     juce::OwnedArray<juce::PluginDescription> results;
     for (auto it : manager.getFormats()) it->findAllTypesForFile(results, juce::StringArray(argv + 1, argc - 1).joinIntoString(" "));
-    if (results.isEmpty()) return 1;
+    printf("%d\n", results.size());
+    if (results.isEmpty()) return 0;
     juce::XmlElement::TextFormat format;
     format.newLineChars = nullptr;
     format.addDefaultHeader = false;
