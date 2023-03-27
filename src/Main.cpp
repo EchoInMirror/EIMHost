7f266d3e96920484b9f2b47f780182409d1d4035
xris1658 <chris1658@foxmail.com>
2023-03-27 18:47:33 +0800

    Fix traversing operation; prevent crash on writing parameter changes

--- src/Main.cpp
+++ src/Main.cpp
@@ -269,13 +269,13 @@ private:
 
     void writeAllParameterChanges() {
         auto time = juce::Time::getApproximateMillisecondCounter();
-        for (auto it = parameterChanges.begin(); it != parameterChanges.end(); it++) {
+        for (auto it = parameterChanges.begin(); it != parameterChanges.end(); ++it) {
 			if (it->second.second > time) continue;
             writeCerr((char)3);
             writeCerr(it->first);
             writeCerr(it->second.first);
-            parameterChanges.erase(it);
         }
+        parameterChanges.clear();
     }
 
     template <typename T> inline void writeToHostBuffer(T var) {
